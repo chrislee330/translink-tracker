4bec4880fbd69dd98e84b54f9821396992a5903a
clee330 <clee330@students.cs.ubc.ca>
2026-05-30 14:06:41 -0700

    feat: add vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
+import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights'
+
+// vercel analytics and speed 
+inject()
+injectSpeedInsights()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
